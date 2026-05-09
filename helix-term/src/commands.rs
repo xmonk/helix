e07d8dbffbf75a6749c87ecb8824137a2a6591d4
Juan Fuentes <jj@xmonk.org>
2026-05-09 08:41:12 -0400

    remove unused variable

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -2481,7 +2481,6 @@ fn search_next_or_prev_impl(cx: &mut Context, movement: Movement, direction: Dir
         } else {
             false
         };
-        let wrap_around = search_config.wrap_around;
         let is_crlf = doc!(cx.editor).line_ending == LineEnding::Crlf;
         if let Ok(regex) = rope::RegexBuilder::new()
             .syntax(
