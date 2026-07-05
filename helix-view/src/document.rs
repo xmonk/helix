821b3580b8de73fb91e94c9a2554c2a473ba011c
Juan Fuentes <jj@xmonk.org>
2026-07-05 11:47:29 -0400

    fix merge conflicts

--- helix-view/src/document.rs
+++ helix-view/src/document.rs
@@ -2013,8 +2013,8 @@ impl Document {
     }
 
     pub fn diff_handle(&self) -> Option<&DiffHandle> {
-         self.diff_handle.as_ref()
-     }
+        self.diff_handle.as_ref()
+    }
 
     /// Intialize/updates the differ for this document with a new base.
     pub fn set_diff_base(&mut self, diff_base: Vec<u8>) {
